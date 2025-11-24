dc4114c134be03ee6549416dadbb4e0e6e771681
Abd-El-Rahman-Mohamed <webdevelopmentlj@gmail.com>
2025-11-24 21:42:13 +0200

    Some animation for the cards in the home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,7 +86,7 @@ const navLinks = document.querySelectorAll('.nav-menu a');
 
 navLinks.forEach(link => {
     if (link.getAttribute('href') === currentLocation || 
-        (currentLocation.includes(link.getAttribute('href')) && link.getAttribute('href') !== 'index.html')) {
+        (currentLocation.includes(link.getAttribute('href')) && link.getAttribute('href') !== './')) {
         link.classList.add('active');
     }
 });
